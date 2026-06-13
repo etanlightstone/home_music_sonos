44805da7378e9617fbab760a384c32cbe9893fd7
Etan Lightstone <etan@lightstone.io>
2026-06-13 11:38:53 -0700

    phase 1 done I think

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
+from jinja2 import Environment, FileSystemLoader
 from contextlib import asynccontextmanager
 
 from database import init_db
@@ -15,10 +15,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="SonosWeb", lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+
+jinja_env = Environment(loader=FileSystemLoader("templates"), autoescape=False)
+jinja_env.cache = None
+templates = Jinja2Templates(env=jinja_env)
 
 app.include_router(settings_router.router, prefix="/api/settings", tags=["settings"])
 
 @app.get("/")
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    # NOTE: TemplateResponse signature is (request, name, context) - request comes FIRST
+    return templates.TemplateResponse(request, "index.html")
